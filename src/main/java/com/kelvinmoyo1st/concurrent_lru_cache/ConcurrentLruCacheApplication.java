b4b7949fbb41ad2fbf881bc5d90af6fc3d7e1a91
kelvinmoyo1st <kelvinmoyo1st@gmail.com>
2026-07-17 09:25:32 +0000

    Initial Spring Boot skeleton

---
+++ src/main/java/com/kelvinmoyo1st/concurrent_lru_cache/ConcurrentLruCacheApplication.java
@@ -0,0 +1,13 @@
+package com.kelvinmoyo1st.concurrent_lru_cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrentLruCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrentLruCacheApplication.class, args);
+	}
+
+}
